4bb911edd9298a26790ecdfb9ee9d425043be3fb
Fernando Miramontes <fernandomiramontes03@gmail.com>
2023-02-23 16:24:36 -0600

    first commit

---
+++ pmap02/javascript/geolocation.js
@@ -0,0 +1,89 @@
+import Feature from "ol/Feature.js";
+import Geolocation from "ol/Geolocation.js";
+import Map from "ol/Map.js";
+import Point from "ol/geom/Point.js";
+import View from "ol/View.js";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style.js";
+import { OSM, Vector as VectorSource } from "ol/source.js";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
+
+const view = new View({
+  center: [0, 0],
+  zoom: 2,
+});
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: "map",
+  view: view,
+});
+
+const geolocation = new Geolocation({
+  // enableHighAccuracy must be set to true to have the heading value.
+  trackingOptions: {
+    enableHighAccuracy: true,
+  },
+  projection: view.getProjection(),
+});
+
+function el(id) {
+  return document.getElementById(id);
+}
+
+el("track").addEventListener("change", function () {
+  geolocation.setTracking(this.checked);
+});
+
+// update the HTML page when the position changes.
+geolocation.on("change", function () {
+  el("accuracy").innerText = geolocation.getAccuracy() + " [m]";
+  el("altitude").innerText = geolocation.getAltitude() + " [m]";
+  el("altitudeAccuracy").innerText = geolocation.getAltitudeAccuracy() + " [m]";
+  el("heading").innerText = geolocation.getHeading() + " [rad]";
+  el("speed").innerText = geolocation.getSpeed() + " [m/s]";
+  el("coordinates").innerText = geolocation.getPosition() + " [l/l]";
+});
+
+// handle geolocation error.
+geolocation.on("error", function (error) {
+  const info = document.getElementById("info");
+  info.innerHTML = error.message;
+  info.style.display = "";
+});
+
+const accuracyFeature = new Feature();
+geolocation.on("change:accuracyGeometry", function () {
+  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+const positionFeature = new Feature();
+positionFeature.setStyle(
+  new Style({
+    image: new CircleStyle({
+      radius: 6,
+      fill: new Fill({
+        color: "#3399CC",
+      }),
+      stroke: new Stroke({
+        color: "#fff",
+        width: 2,
+      }),
+    }),
+  })
+);
+
+geolocation.on("change:position", function () {
+  const coordinates = geolocation.getPosition();
+  positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
+});
+
+new VectorLayer({
+  map: map,
+  source: new VectorSource({
+    features: [accuracyFeature, positionFeature],
+  }),
+});
